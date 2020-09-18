95d50f47e9eb8e50f48cc84ccbe839716925a95f
90034 <70871848+90034@users.noreply.github.com>
2020-09-18 23:46:13 +0800

    Create 第二章.py

---
+++ 第二章.py
@@ -0,0 +1,15 @@
+string='I love you'
+print(string[0:1])
+string="I love you"
+print(string[2:6])
+string="I love you"
+print(string[7:10])
+
+
+print(string[::-1])
+
+print(string+' so'*100+' much')
+
+import random
+x=random.randint(1,100)
+print(string+' so'*x+' much')
